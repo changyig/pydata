17eb07a353abbec88f5eb3e0dbcf7882391e96d4
CYG <15716365368@163.com>
2021-01-16 16:20:17 +0800

    2021-01-16 数据展示 和mysql类功能修改

--- work/data_show.py
+++ work/data_show.py
@@ -1,10 +1,13 @@
+# coding:utf-8
 import matplotlib.pyplot as plt
 import time
 import os
 import pymysql
 import math
+from mysql_class import Mysql
 class Data:
-    def __init__(self):
+    def __init__(self,mysql=None):
+        self.mysql = mysql
         self.connect = False
         self.connect = pymysql.connect(
             host="127.0.0.1",
@@ -18,9 +21,10 @@ class Data:
         self.start_time = time.time()
         self.currentLine = 0
         self.num = 0  # 插入数据库的数量
-        self.website = 'wettstein-transporte.ch'  # 网站域名
+        self.website = 'kawiarniapinokio.pl'  # 网站域名
         self.cursor = self.connect.cursor()
-        self.bie_or_bar = 1 #1:bie 2:bar
+        self.bie_or_bar = 2 #1:bie 2:bar
+    #获取指定表格的总得数量
     def get_num(self):
         # sql = "select type,count(*) as num,origin from all_keyword_data where origin like '%transports-speciaux.ch%' GROUP BY type"
         origin_value ="'%"+self.website+"%'"
@@ -28,6 +32,7 @@ class Data:
         self.cursor.execute(sql)
         res=self.cursor.fetchall()
         return res
+    #从数据库中调用相应的数据
     def get_data(self):
         origin_value = "'%" + self.website + "%'"
         if self.bie_or_bar==1:
@@ -39,6 +44,8 @@ class Data:
         res=self.cursor.fetchall()
         return res
         pass
+
+    #条柱状数据显示
     def bar_show(self):
         x=[]
         y=[]
@@ -56,6 +63,8 @@ class Data:
         pass
     def plot_show(self):
         pass
+
+    #扇形数据显示
     # //1 => 'jaw crusher', 2 => 'impact crusher', 3 => 'cone crusher', 4 => 'stone crusher', 5 => 'mobile crusher',
     # 6 => 'crusher', 8 => 'hammer mill', 9 => 'ball mill', 10 => 'grinding mill', 13 => 'sand making machine', 14 => 'drying machine'
     def pie_show(self):
@@ -77,11 +86,34 @@ class Data:
                 startangle=90)  # startangle控制饼状图的旋转方向
         plt.axis('equal')  # 保证饼状图是正圆，否则会有一点角度偏斜
         plt.show()
+    #物料词和非物料词扇形图
+    def pie_show2(self):
+        #物料+产品  物料+非产品 非物料+产品 非物料+非产品
+        num=[]
+        num1=self.mysql.table('all_keyword_data').where([{'origin':['like',self.website]},{'material_score':['!=',0]},{'type':['!=',0]}]).count()
+        num2=self.mysql.table('all_keyword_data').where([{'origin':['like',self.website]},{'material_score':['!=',0]},{'type':['=',0]}]).count()
+        num3=self.mysql.table('all_keyword_data').where([{'origin':['like',self.website]},{'material_score':['=',0]},{'type':['!=',0]}]).count()
+        num4=self.mysql.table('all_keyword_data').where([{'origin':['like',self.website]},{'material_score':['=',0]},{'type':['=',0]}]).count()
+        num.extend([num1,num2,num3,num4])
+        print(num)
+        x = []
+        y = ['物料+产品','物料+非产品','非物料+产品','非物料+非产品']
+        for i in num:
+            x.append(i[0])
+        plt.title(self.website+'词的分类分析')
+        plt.rcParams['font.sans-serif'] = ['SimHei']
+        plt.rcParams['axes.unicode_minus'] = False
+        plt.pie(x,labels=y,autopct='%1.1f%%',shadow=True,
+                startangle=90)  # startangle控制饼状图的旋转方向
+        plt.axis('equal')  # 保证饼状图是正圆，否则会有一点角度偏斜
+        # plt.figure("bie")
+        plt.show()
 if __name__ == '__main__':
-    data = Data()
+    mysql = Mysql(dbname='industro')
+    data = Data(mysql)
+    # data.pie_show()
     if data.bie_or_bar==1:
         data.pie_show()
     else:
         data.bar_show()
-    # data.pie_show()
     print('结束执行')
--- work/text_insert_mysql.py
+++ work/text_insert_mysql.py
@@ -19,14 +19,14 @@ class Mysql:
         # 通过cursor执行增删查改
         self.cursor = self.connect.cursor()
     def read_txt_mysql(self):#（0 未知 1是地区词 2 不是地区词）
-        origin='uwojciakow.pl'
-        filename= r"./scrapy_data/uwojciakow.pl.txt"
+        origin='kawiarniapinokio.pl'
+        filename= r"./scrapy_data/kawiarniapinokio.pl.txt"
         country=0
         remark='排名比较好的网站'
         with open(filename, 'r',encoding='utf-8') as infile:
             for line in infile:
                 try:
-                    sql = "INSERT INTO all_keyword(keyword,origin,createtime,country,remark) VALUES(%s,%s,%s,%s,%s)"
+                    sql = "INSERT INTO all_keyword_data(keyword,origin,createtime,country,remark) VALUES(%s,%s,%s,%s,%s)"
                     self.cursor.execute(sql, (self.filter_space(line), origin, int(time.time()),country,remark))
                     self.cursor.connection.commit()
                     self.mysqlNum = self.mysqlNum + 1
